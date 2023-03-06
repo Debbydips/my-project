112fb407a6c45e6a0852e80b89d790215bcf1a65
Deborah <technocraterpdevelopers@gmail.com>
2023-03-06 16:50:18 +0100

    app.component.css changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,16 +9,42 @@ export class AppComponent {
   newMemberName ='';
   members: string[] =[];
   errorMessage ='';
-
+  numberOfWorkers: number | '' = ''
+  
+  
   onInput(member: string) {
     this.newMemberName= member;
-    console.log ();
-    
+  }
+
+  onNumberOfWorkersInput(value: string){
+    this.numberOfWorkers = Number(value);
   }
 
   addMember() {
+
+    if( !this.newMemberName){
+      this.errorMessage = "Name can't be empty";
+      return;
+
+    }
+
     this.members.push(this.newMemberName);
-   
+    this.newMemberName = '';
+    
+  
   }
   
+  generateWorkers() {
+
+    if( !this.numberOfWorkers || this.numberOfWorkers <=0){
+      return
+    }
+
+    const allMembers =[...this.members]
+
+    for( let i = 0; i < this.numberOfWorkers; i++){
+     const randomIndex = Math.floor(Math.random () * allMembers.length);
+    }
+     console.log(randomIndex);
+  }
 }
